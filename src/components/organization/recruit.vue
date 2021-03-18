b4722ba0e6615a573fb005243d57fb27fd3822d7
shazhiya <2691032513@qq.com>
2021-03-18 23:43:30 +0800

    recruit

--- src/components/organization/recruit.vue
+++ src/components/organization/recruit.vue
@@ -6,19 +6,23 @@
                 <van-cell-group title="角色权限管理">
                     <cc>
                         <div style="position: relative" v-for="role in roles" :key="role.roleName">
-                            <div style="position: absolute; top: 5px; right: 5px">
+                            <div style="position: absolute; top: 5px; right: 5px" @click="delRole(role.roleId)">
                                 <van-icon name="delete-o"></van-icon>
                             </div>
                             <van-row style="padding: 10px; border-bottom: 1px #dddddd solid">
-                                <van-col span="6">
-                                    <van-tag size="large" :color="role.tagColor" style="margin: 10px">{{role.roleName}}</van-tag>
-                                </van-col>
-                                <van-col span="15" offset="1">
-                                    <van-tag v-for="security in role.securities" :key="security.securityName" plain style="margin: 10px 5px" closeable @close="removeSecurity(role.roleId,security.securityId)">
-                                        {{security.securityName}}
+                                <van-col span="7">
+                                    <van-tag size="large" :color="role.tagColor" style="margin: 10px">
+                                        {{ role.roleName }}
                                     </van-tag>
-                                    <van-tag style="margin: 10px 5px;" type="primary">
-                                        <div style="padding: 0 10px" @click="selectSecurity=true,currentRole=role.roleId">选择</div>
+                                </van-col>
+                                <van-col span="14" offset="1">
+                                    <van-tag v-for="security in role.securities" :key="security.securityName" plain style="margin: 10px 5px" closeable @close="removeSecurity(role.roleId,security.securityId)">
+                                        {{ security.securityName }}
+                                    </van-tag>
+                                    <van-tag style="margin: 10px 5px;" mark type="primary">
+                                        <div style="padding: 0 10px" @click="currentRole=role.roleId,selectSecurity=true">
+                                            编辑权限
+                                        </div>
                                     </van-tag>
                                 </van-col>
                             </van-row>
@@ -30,30 +34,9 @@
                     </cc>
                 </van-cell-group>
 
-                <van-cell-group title="成员角色审配">
+                <van-cell-group title="成员角色分配配">
                     <cc>
-                        <div style="position: relative">
-                            <van-row>
-                                <van-col span="6">
-                                    <van-tag size="large" style="margin: 10px">教师</van-tag>
-                                </van-col>
-                                <van-col span="15" offset="1">
-                                    <van-tag plain style="margin: 10px 5px" closeable @close="removeSecurity()">招贤纳士
-                                    </van-tag>
-                                    <van-tag style="margin: 10px 5px;" type="primary">
-                                        <div style="padding: 0 10px" @click="selectSecurity=true">选择</div>
-                                    </van-tag>
-                                </van-col>
-                            </van-row>
-                            <div style="position: absolute; top: 0; right: 0">
-                                <van-icon name="delete-o"></van-icon>
-                            </div>
-                        </div>
-
-                        <van-button style="width: 100%; border: none; background:none;" size="mini"
-                                    @click="openAddRole">
-                            添加角色
-                        </van-button>
+                        
                     </cc>
                 </van-cell-group>
 
@@ -61,21 +44,21 @@
         </navbar>
 
         <van-action-sheet
-            v-model="selectSecurity"
-            :actions="actions"
-            cancel-text="取消"
-            close-on-popstate
-            description="分配权限"
-            @select="clickSecurity"
-            @open="open"
-            @close="close"
+                v-model="selectSecurity"
+                :actions="securities"
+                cancel-text="取消"
+                close-on-popstate
+                description="分配权限"
+                @select="clickSecurity"
+                @open="open"
+                @close="close"
         />
 
         <van-dialog
-            title="添加角色"
-            v-model="addRole"
-            show-cancel-button
-            :before-close="createRole"
+                title="添加角色"
+                v-model="addRole"
+                show-cancel-button
+                :before-close="createRole"
         >
             <van-field v-model="roleName" label="角色名" placeholder="角色名"></van-field>
             <van-field v-model="tagColor" label="标签颜色" type="color"></van-field>
@@ -93,68 +76,88 @@ export default {
     data() {
         return {
             organization: {
-                organizationId: this.$route.params.organizationId||1
+                organizationId: this.$route.params.organizationId || 1
             },
             selectSecurity: false,
             addRole: false,
             roleName: '',
             tagColor: '',
             currentRole: '',
-            actions: this.$store.state.allSecurity.map(sec=>{return {
-                name: sec.securityName,
-                securityId: sec.securityId
-            }})
+            securities: this.$store.state.allSecurity.map(sec => {
+                return {
+                    name: sec.securityName,
+                    securityId: sec.securityId
+                }
+            })
         }
     },
     methods: {
-        removeSecurity(roleId,securityId) {
-            console.log(roleId,securityId)
+        removeSecurity(roleId, securityId) {
+            this.delSecurity(roleId, securityId).then(() => this.loadTheOrganization())
         },
         clickSecurity(action) {
-            this.$set(action, 'color', !action.color ? 'red' : undefined)
-            this.addSecurity(this.currentRole,action.securityId)
+            if (action.color) this.delSecurity(this.currentRole, action.securityId).then(() => this.$set(action, 'color', !action.color ? 'red' : undefined))
+            else this.addSecurity(this.currentRole, action.securityId).then(() => this.$set(action, 'color', !action.color ? 'red' : undefined))
         },
         openAddRole() {
             this.addRole = true
         },
         createRole(action, done) {
-            if (action==='confirm'){
+            if (action === 'confirm') {
                 post('organization/updateRole/addRole'
-                    ,{organization: {organizationId:this.organization.organizationId},role: {roleName:this.roleName,tagColor:this.tagColor}}
-                        ,res=>{
-                        if (res.data){
-                            this.$notify({type:'success',message:'添加成功'})
+                    , {
+                        organization: {organizationId: this.organization.organizationId},
+                        role: {roleName: this.roleName, tagColor: this.tagColor}
+                    }
+                    , res => {
+                        if (res.data) {
+                            this.$notify({type: 'success', message: '添加成功'})
                             this.loadTheOrganization()
                             done()
                         }
                     })
-            }else {
+            } else {
                 done()
             }
         },
-        loadTheOrganization(){
-            resolvedPost('organization/load',{organizationId:this.organization.organizationId,status:'有效'},)
-            .then(res=>{
-                this.organization = res[0]
+        loadTheOrganization() {
+            resolvedPost('organization/load', {organizationId: this.organization.organizationId, status: '有效'})
+                .then(res => {
+                    this.organization = res[0]
+                })
+        },
+        open() {
+            let has = this.organization.uros.find(uro => uro.role.roleId === this.currentRole).role.securities
+            this.securities.forEach(security => {
+                if (has.some(h => h.securityId === security.securityId)) this.$set(security, 'color', 'red')
+                else this.$set(security, 'color', undefined)
             })
         },
-        open(){},
-        close(){
-
+        close() {
+            this.loadTheOrganization()
         },
-        addSecurity(roleId,securityId){
-            return post('organization/updateRole/addSecurityToRole',{role:{roleId,securities:[{securityId}]}})
-            .then(res=>{
-                console.log(res)
-            })
+        addSecurity(roleId, securityId) {
+            return post('organization/updateRole/addSecurityToRole', {role: {roleId, securities: [{securityId}]}})
+        },
+        delSecurity(roleId, securityId) {
+            return post('organization/updateRole/delSecurityToRole', {role: {roleId, securities: [{securityId}]}})
+        },
+        delRole(roleId) {
+            this.$dialog
+                .confirm({message:'确认删除角色？'})
+                .then(()=>{
+                    post('organization/updateRole/delRole', {role: {roleId}})
+                        .then(() => this.loadTheOrganization())
+                })
+                .catch(()=>{})
         }
     },
-    mounted(){
+    mounted() {
         this.loadTheOrganization()
     },
-    computed:{
-        roles(){
-            return this.organization?.uros?.filter(uro=>!uro.user).map(uro=>uro.role)
+    computed: {
+        roles() {
+            return this.organization?.uros?.filter(uro => !uro.user).map(uro => uro.role)
         }
     }
 }
